2bc6553f6b56aeddd133e3bffde8c896678efc99
Christoph Schultheiss <cschulth@math.ethz.ch>
2020-12-22 17:35:17 +0100

    Clean up simulation file for multicarving intervals

--- multi_ci.R
+++ multi_ci.R
@@ -1,18 +1,12 @@
 rm(list = ls(all = TRUE))
-local <- FALSE # on local machine or on D-MATH server
-save <- FALSE
-if (!save && !local) {
-  save <- TRUE
-  warning("Do not use cluster without saving, save was set to TRUE")
-}
+save <- TRUE
+
 # create save location, adjust depending on folder structure
+
 if (save) {
   newdir <- format(Sys.time(), "%d-%b-%Y %H.%M")
-  if (local) {
-    dir.create(paste("C:/Users/Christoph/Documents/ETH/MA/R_trials/multi_carve/", newdir, sep = ""))
-  } else {
-    dir.create(paste("multi_carve/",newdir,sep="")) 
-  }
+  dir.create(paste("multi_carve/",newdir,sep="")) 
+
 }
 
 require(MASS)
@@ -26,23 +20,17 @@ require(parallel)
 require(doRNG)
 require(tmg)
 require(truncnorm)
+require(git2r)
+
+commit <- revparse_single(revision = "HEAD")
+print(paste("Run on commit", commit$sha, 'i.e.:', commit$summary))
 
 
-if (local) {
-  # adjust depending on folder structure
-  source("C:/Users/Christoph/Documents/ETH/MA/R_trials/hdi_adjustments.R")
-  source("C:/Users/Christoph/Documents/ETH/MA/R_trials/optimal_inference.R")
-  source("C:/Users/Christoph/Documents/ETH/MA/R_trials/tryCatch-W-E.R")
-} else {
-  source("hdi_adjustments.R")
-  source("optimal_inference.R")
-  source("tryCatch-W-E.R")
-}
 
-if (local) {
-  library(tcltk)
-}
-
+# adjust depending on folder structure
+source("hdi_adjustments.R")
+source("optimal_inference.R")
+source("tryCatch-W-E.R")
 
 # toeplitz
 n <- 100
@@ -58,26 +46,15 @@ set.seed(42) # to make different methods comparable, fix the x-matrix
 x <- mvrnorm(n, rep(0, p), Cov) 
 # should create the right x on D-MATH server, x[1 ,1] = 0.958
 
-# if x-data is presaved
-# if (local) {
-# load("~/ETH/MA/R_trials/x.RData")
-# } else {
-#   load("x.RData")
-# }
-y_true <- x %*% beta
-sigma <- 2
-report_sigma <- FALSE
 
-frac_vec <- c(0.5, 0.75, 0.9, 0.95, 0.99) # selection fraction
+y.true <- x %*% beta
+sigma <- 2
+
+frac.vec <- c(0.5, 0.75, 0.9, 0.95, 0.99) # selection fraction
 nsim <- 200
 ntasks <- nsim
 progress <- function(n, tag) {
-  mod <- if (local) 4
-  else
-    16
-  if (local) {
-    setTkProgressBar(pb, n)
-  }
+  mod <- 16
   if (n %% mod == 0 ) {
     cat(sprintf('tasks completed: %d; tag: %d\n', n, tag))
   }
@@ -88,165 +65,146 @@ progress <- function(n, tag) {
 }
 RNGkind("L'Ecuyer-CMRG")
 set.seed(42)
-seed_v <- sample(1:10000,length(frac_vec))
-print(seed_v) # 3588 3052 2252 5257 8307
-seed_n <- 0
-usei <- TRUE # should sigma_hat be calculated modelwise
+seed.vec <- sample(1:10000, length(frac.vec))
+print(seed.vec) # 3588 3052 2252 5257 8307
+seed.n <- 0
 B <- 50
-for (frac in frac_vec) {
-  seed_n <- seed_n+1
-  set.seed(seed_v[seed_n])
+for (frac in frac.vec) {
+  seed.n <- seed.n+1
+  set.seed(seed.vec[seed.n])
   # check set-up
-  print(usei)
   print(frac)
   print(B)
-  print(report_sigma)
-  if (local) {
-    pb <- tkProgressBar(max = ntasks, title = paste("B=", B, " split=", frac))
-  }
   opts <- list(progress = progress)
   
   # parallelization
-  if (local) {
-    cl<-makeSOCKcluster(4)
-  } else {
-    cl<-makeSOCKcluster(16) 
-  }
-  rseed <- seed_v[seed_n]
-  clusterSetRNGStream(cl, iseed = rseed) #make things reproducible
+  # choose different number of cores if wished
+  cl <- makeSOCKcluster(16) 
+
+  rseed <- seed.vec[seed.n]
+  clusterSetRNGStream(cl, iseed = rseed) # make things reproducible
   registerDoSNOW(cl)
   tic()
-  res<-foreach(gu=1:nsim, .combine = rbind,
-               .packages = c("MASS","selectiveInference","glmnet","Matrix",
-                             "hdi","tmg","truncnorm"),.options.snow=opts) %dorng%{
+  res<-foreach(gu = 1:nsim, .combine = rbind,
+               .packages = c("MASS", "selectiveInference", "glmnet", "Matrix",
+                             "hdi", "tmg", "truncnorm"), .options.snow=opts) %dorng%{
    # alternative if sequential computation is preferred
-   # res<-foreach(gu=1:nsim,.combine = rbind) %do%{
-    y_true <- x %*% beta
-    y <- y_true + sigma * rnorm(n)
-    reported_sigma <- NA
+   # res <- foreach(gu = 1:nsim, .combine = rbind) %do%{
+    y.true <- x %*% beta
+    y <- y.true + sigma * rnorm(n)
     gammavec <- round(seq(ceiling(0.05 * B) / B, 1 - 1 / B, by = 1 / B), 2)
     gammavec[1] <- 0.05999 # due to inconsistency for multisplitting CI
     mcrtry <- tryCatch_W_E(multi.carve.ci.saturated(x, y, B = B, fraction = frac, ci.level = 0.95, 
-                                     model.selector = lasso.cvcoef, classical.fit = lm.pval.flex,
+                                     model.selector = lasso.cvcoef, classical.fit = lm.pval,
                                      parallel = FALSE, ncores = getOption("mc.cores", 2L), gamma = gammavec,
                                      args.model.selector = list(standardize = FALSE, intercept = TRUE,
-                                                                tol.beta = 0, use_lambda.min = FALSE),
-                                     args.classical.fit = list(Sigma = reported_sigma),
-                                     verbose = FALSE, ci.timeout = 10, FWER = FALSE, split_pval = TRUE,
-                                     return.selmodels = TRUE, return.nonaggr = TRUE,
-                                     args.lasso.inference = list(sigma = reported_sigma, verbose = FALSE)),0)
+                                                                tol.beta = 0, use.lambda.min = FALSE),
+                                     verbose = FALSE, ci.timeout = 10, FWER = FALSE, split.pval = TRUE,
+                                     return.selmodels = TRUE, return.nonaggr = TRUE), 0)
     
+    out.list <- list()
+    out.list$y <- y
     if(!is.null(mcrtry$error)) {
       err <- paste("mcr:", mcrtry$error)
       war <- if (is.null(mcrtry$warning)) NA
       else c(mcrtry$warning)
-      out_list <- list()
-      out_list$splitlci <- rep(NA,p)
-      out_list$splituci <- rep(NA,p)
-      out_list$carvelci <- rep(NA,p)
-      out_list$carveuci <- rep(NA,p)
-      out_list$betamin <- rep(NA,p)
-      out_list$betamax <- rep(NA,p)
-      out_list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
-      out_list
+      out.list$split.lci <- rep(NA,p)
+      out.list$split.uci <- rep(NA,p)
+      out.list$carve.lci <- rep(NA,p)
+      out.list$carve.uci <- rep(NA,p)
+      out.list$beta.min <- rep(NA,p)
+      out.list$beta.max <- rep(NA,p)
+      out.list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
+      out.list
     } else {
       mcr <- mcrtry$value
       
-      betamin <- rep(-Inf,p)
-      betamax <- rep(Inf,p)
+      beta.min <- rep(-Inf, p)
+      beta.max <- rep(Inf, p)
       for (j in 1:p) {
-        betas <- rep(NA,50)
+        betas <- rep(NA, 50)
         for (i in 1:B) {
           if (mcr[[1]]$sel.models[i, j]) {
             mod <- which(mcr[[1]]$sel.models[i, ])
             coefn <- which(mod == j)
-            xsub <- cbind(rep(1, n), x[, mod])
-            betas[i] <- (ginv(xsub) %*% y_true)[coefn + 1]
+            x.sub <- cbind(rep(1, n), x[, mod])
+            betas[i] <- (ginv(x.sub) %*% y.true)[coefn + 1]
           }
         }
         if (any(!is.na(betas))) {
-          betamin[j] <- min(betas, na.rm = TRUE)
-          betamax[j] <- max(betas, na.rm = TRUE)
+          beta.min[j] <- min(betas, na.rm = TRUE)
+          beta.max[j] <- max(betas, na.rm = TRUE)
         }
       }
-      slci <- mcr[[2]]$lci
-      suci <- mcr[[2]]$uci
-      clci <- mcr[[1]]$lci
-      cuci <- mcr[[1]]$uci
 
-      out_list <- list()
-      out_list$splitlci <- slci
-      out_list$splituci <- suci
-      out_list$carvelci <- clci
-      out_list$carveuci <- cuci
-      out_list$betamin <- betamin
-      out_list$betamax <- betamax
+      out.list$split.lci <- mcr[[2]]$lci # lower end of splitting interval
+      out.list$split.uci <- mcr[[2]]$uci # upper end of splitting interval
+      out.list$carve.lci <- mcr[[1]]$lci # lower end of carving interval
+      out.list$carve.uci <- mcr[[1]]$uci # upper end of carving interval
+      out.list$beta.min <- beta.min
+      out.list$beta.max <- beta.max
       err <- if (is.null(mcrtry$error)) NA
       else c(mcrtry$error) # should not happen due to earlier check
       war <- if (is.null(mcrtry$warning)) NA
       else c(mcrtry$warning)
-      out_list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
-      out_list
+      out.list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
+      out.list
       # end of simulation run
     }
     # end of simulation for given fraction
   }  
   toc()
   stopCluster(cl)
-  if (local) {
-    close(pb)
-  }
 
+  # analyse results for given fraction
+  # get matrix of errors and warnings
   expmatr <- matrix(unlist(res[, "exception"]),nrow = dim(res)[1],
                     ncol = 2,byrow = TRUE)
   print(sum(is.na(expmatr[, 1])))
   succ <- which(is.na(expmatr[, 1]))
   print("succesful runs")
   
-  splitlci <- matrix(unlist(res[, "splitlci"]), nrow = dim(res)[1],
+  all.y <- matrix(unlist(res[,"y"]), nrow = dim(res), byrow = TRUE)
+  sd <- attr(res, "rng")
+  
+  split.lci <- matrix(unlist(res[, "split.lci"]), nrow = dim(res)[1],
                      ncol = p, byrow = TRUE)
-  splituci <- matrix(unlist(res[, "splituci"]), nrow = dim(res)[1],
+  split.uci <- matrix(unlist(res[, "split.uci"]), nrow = dim(res)[1],
                      ncol = p, byrow = TRUE)
-  carvelci <- matrix(unlist(res[, "carvelci"]), nrow = dim(res)[1],
+  carve.lci <- matrix(unlist(res[, "carve.lci"]), nrow = dim(res)[1],
                      ncol = p, byrow = TRUE)
-  carveuci <- matrix(unlist(res[, "carveuci"]), nrow = dim(res)[1],
+  carve.uci <- matrix(unlist(res[, "carve.uci"]), nrow = dim(res)[1],
                      ncol = p, byrow = TRUE)
-  betamin <- matrix(unlist(res[, "betamin"]), nrow = dim(res)[1],
+  beta.min <- matrix(unlist(res[, "beta.min"]), nrow = dim(res)[1],
                     ncol = p, byrow = TRUE)
-  betamax <- matrix(unlist(res[, "betamax"]), nrow = dim(res)[1],
+  beta.max <- matrix(unlist(res[, "beta.max"]), nrow = dim(res)[1],
                     ncol = p, byrow = TRUE)
   
-  colnames(splitlci) <- paste(1:p)
-  colnames(splituci) <- paste(1:p)
-  colnames(carvelci) <- paste(1:p)
-  colnames(carveuci) <- paste(1:p)
-  colnames(betamin) <- paste(1:p)
-  colnames(betamax) <- paste(1:p)
-  spliterr <- (splitlci > betamin | splituci < betamax)
-  print(mean(apply(spliterr, 1, sum)))
-  carveerr <- (carvelci > betamin | carveuci < betamax)
-  print(mean(apply(carveerr, 1, sum)))
-  splitdis <- splituci - splitlci
-  splitdis[spliterr] <- NA
-  carvedis <- carveuci - carvelci
-  carvedis[carveerr] <- NA
-  print(quantile(splitdis, seq(0.01, 0.1, 0.01), na.rm = TRUE))
-  print(quantile(carvedis, seq(0.01, 0.1, 0.01), na.rm = TRUE))
-  results <- list("splitlci" = splitlci, "splituci" = splituci, "carvelci" = carvelci,
-                  "carveuci" = carveuci, "betamin" = betamin, "betamax" = betamax)
+  colnames(split.lci) <- paste(1:p)
+  colnames(split.uci) <- paste(1:p)
+  colnames(carve.lci) <- paste(1:p)
+  colnames(carve.uci) <- paste(1:p)
+  colnames(beta.min) <- paste(1:p)
+  colnames(beta.max) <- paste(1:p)
+  split.err <- (split.lci > beta.min | split.uci < beta.max)
+  print(mean(apply(split.err, 1, sum))) # average number of false coverage per run for splitting
+  carve.err <- (carve.lci > beta.min | carve.uci < beta.max)
+  print(mean(apply(carve.err, 1, sum))) # average number of false coverage per run for carving
+  split.dis <- split.uci - split.lci
+  split.dis[split.err] <- NA
+  carve.dis <- carve.uci - carve.lci
+  carve.dis[carve.err] <- NA
+  print(quantile(split.dis, seq(0.01, 0.1, 0.01), na.rm = TRUE)) # quantile of interval lengths for covering intervals
+  print(quantile(carve.dis, seq(0.01, 0.1, 0.01), na.rm = TRUE))
+  results <- list("split.lci" = split.lci, "split.uci" = split.uci, "carve.lci" = carve.lci,
+                  "carve.uci" = carve.uci, "beta.min" = beta.min, "beta.max" = beta.max)
   
-  simulation <- list("results" = results, "exceptions" = expmatr, "B" = B,
-                     "split" = frac, "nsim" = nsim, "seed" = rseed)
+  simulation <- list("results" = results, "exceptions" = expmatr, "y" = all.y, "B" = B,
+                     "split" = frac, "nsim" = nsim, "seed" = rseed,
+                     "sd" = sd, "commit" = commit)
   resname <- paste0("results ", format(Sys.time(), "%d-%b-%Y %H.%M"), " split=", frac, " B=", B, " seed=", rseed)
   # adjust depending on folder structure
-  if (save) {
-    if (local) {
-      save(simulation, file = paste("C:/Users/Christoph/Documents/ETH/MA/R_trials/multi_carve/",
-                                    newdir, "/", resname, ".RData", sep = ""))
-    } else {
-      save(simulation,file = paste("multi_carve/", newdir, "/", resname, ".RData", sep = ""))
-    }
-  }
+  if (save) save(simulation, file = paste("multi_carve/", newdir, "/", resname, ".RData", sep = ""))
 }
 
       
