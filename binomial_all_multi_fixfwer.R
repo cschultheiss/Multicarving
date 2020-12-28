b71f14593678649f8bd4816fad75ecf1eda5ff96
Christoph Schultheiss <cschulth@math.ethz.ch>
2020-12-28 03:19:36 +0100

    Adapt binomial simulation file

--- binomial_all_multi_fixfwer.R
+++ binomial_all_multi_fixfwer.R
@@ -1,19 +1,11 @@
 rm(list = ls(all = TRUE))
-local <- FALSE
-save <- FALSE
-if (!save && !local) {
-  save <- TRUE
-  warning("Do not use cluster without saving, save was set to TRUE")
-}
-# create save location, adjust depending on folder structure
+save <- TRUE
+
 if (save) {
   newdir <- format(Sys.time(), "%d-%b-%Y %H.%M")
-  if (local) {
-    dir.create(paste("C:/Users/Christoph/Documents/ETH/MA/R_trials/multi_carve/",newdir,sep = ""))
-  } else {
-    dir.create(paste("multi_carve/",newdir,sep="")) 
-  }
+  dir.create(paste("multi_carve/",newdir,sep="")) 
 }
+
 require(MASS)
 require(glmnet)
 require(Matrix)
@@ -24,22 +16,18 @@ require(doSNOW)
 require(parallel)
 require(doRNG)
 require(tmg)
+require(git2r)
+
+commit <- revparse_single(revision = "HEAD")
+print(paste("Run on commit", commit$sha, 'i.e.:', commit$summary))
 
 
-if (local) {
-  # adjust depending on folder structure
-  source("C:/Users/Christoph/Documents/ETH/MA/R_trials/hdi_adjustments.R")
-  source("C:/Users/Christoph/Documents/ETH/MA/R_trials/optimal_inference.R")
-  source("C:/Users/Christoph/Documents/ETH/MA/R_trials/tryCatch-W-E.R")
-} else {
-  source("hdi_adjustments.R")
-  source("optimal_inference.R")
-  source("tryCatch-W-E.R")
-}
 
-if (local) {
-  library(tcltk)
-}
+# adjust depending on folder structure
+source("hdi_adjustments.R")
+source("optimal_inference.R")
+source("tryCatch-W-E.R")
+source("sample_from_truncated.R")
 
 # toeplitz
 n <- 100
@@ -52,30 +40,20 @@ beta <- rep(0, p)
 beta[sel.index] <- 2
 sparsity <- 5
 set.seed(42) # to make different methods comparable, fix the x-matrix
-x <- mvrnorm(n, rep(0, p), Cov) 
+x <- mvrnorm(n, rep(0, p), Cov)
+print (x[1,1])
 # should create the right x on D-MATH server, x[1 ,1] = 0.958
 
-# # if x-data is presaved
-# if (local) {
-# load("~/ETH/MA/R_trials/x.RData")
-# } else {
-#   load("x.RData")
-# }
 xb <- x %*% beta
-p_true <- exp(xb) / (1 + exp(xb))
+p.true <- exp(xb) / (1 + exp(xb))
 
-B_vec <- c(1, (1:5) * 10) # number of splits
-frac_vec <-c(0.5, 0.75, 0.9, 0.95, 0.99) # selection fraction
+B.vec <- c(1, (1:5) * 10) # number of splits
+frac.vec <- c(0.5, 0.75, 0.9, 0.95, 0.99) # selection fraction
 
 nsim <- 200
 ntasks <- nsim
 progress <- function(n, tag) {
-  mod <- if (local) 4
-  else
-    16
-  if (local) {
-    setTkProgressBar(pb, n)
-  }
+  mod <- 16
   if (n %% mod == 0 ) {
     cat(sprintf('tasks completed: %d; tag: %d\n', n, tag))
   }
@@ -86,124 +64,120 @@ progress <- function(n, tag) {
 }
 RNGkind("L'Ecuyer-CMRG")
 set.seed(42)
-seed_v <- sample(1:10000,length(frac_vec))
-print(seed_v) # 3588 3052 2252 5257 8307
-seed_n <- 0
-B <- max(B_vec)
-for (frac in frac_vec) {
-  seed_n <- seed_n + 1
-  set.seed(seed_v[seed_n])
+seed.v <- sample(1:10000, length(frac.vec))
+print(seed.v) # 3588 3052 2252 5257 8307
+seed.n <- 0
+B <- max(B.vec)
+for (frac in frac.vec) {
+  seed.n <- seed.n + 1
+  set.seed(seed.v[seed.n])
   print(frac)
   print(B)
-  if (local) {
-    pb <- tkProgressBar(max = ntasks, title = paste("B=", B, " split=", frac))
-  }
   opts <- list(progress = progress)
   
-  if (local) {
-    cl <- makeSOCKcluster(4)
-  } else {
-     cl<- makeSOCKcluster(16) 
-  }
-  rseed <- seed_v[seed_n]
+  # parallelization
+  # choose different number of cores if wished
+  cl<- makeSOCKcluster(16) 
+
+  rseed <- seed.v[seed.n]
   clusterSetRNGStream(cl, iseed = rseed) #make things reproducible
   registerDoSNOW(cl)
   tic()
-  res<-foreach(gu=1:nsim, .combine = rbind,
-               .packages = c("MASS","selectiveInference","glmnet","Matrix",
-                             "hdi","tmg","truncnorm"),.options.snow=opts) %dorng%{
+  res<-foreach(gu = 1:nsim, .combine = rbind,
+               .packages = c("MASS", "selectiveInference", "glmnet", "Matrix",
+                             "tmg", "truncnorm", "tictoc") ,.options.snow=opts) %dorng%{
   # alternative if sequential computation is preferred
-  # res<-foreach(gu=1:nsim,.combine = rbind) %do%{
+  # res<-foreach(gu = 1:nsim, .combine = rbind) %do%{
 
     ylim <- runif(n)
     y <- rep(0, n)
-    y[ylim < p_true] <- 1
-
+    y[ylim < p.true] <- 1
 
 
     mcrtry <- tryCatch_W_E(multi.carve(x, y, B = B, fraction = frac, model.selector = lasso.firstqcoef, classical.fit = glm.pval.pseudo,
-                                       parallel = FALSE, ncores = getOption("mc.cores", 2L), gamma = 1,
-                                       args.model.selector = list(standardize = FALSE, q = 16, intercept = TRUE, tol.beta = 0, family="binomial"),
-                                       args.classical.fit =list(), repeat.max = 20, verbose = FALSE, FWER=FALSE, split_pval = TRUE,
-                                       return.selmodels = TRUE, return.nonaggr = TRUE, 
-                                       args.lasso_inference  =list(family = "binomial", selected = TRUE)), 0)
+                                       parallel = FALSE, ncores = getOption("mc.cores", 2L), gamma = 1, skip.variables = FALSE,
+                                       args.model.selector = list(standardize = FALSE, q = 16, intercept = TRUE, tol.beta = 0),
+                                       args.classical.fit =list(), verbose = FALSE, FWER = FALSE, split.pval = TRUE, family = "binomial",
+                                       return.selmodels = TRUE, return.nonaggr = TRUE), 0)
 
     c100try <- tryCatch_W_E(carve100(x, y, model.selector = lasso.firstqcoef,
-                                     args.model.selector = list(standardize = FALSE, q = 16, intercept = TRUE, tol.beta = 1e-5, family = "binomial"),
-                                     repeat.max = 20, verbose = FALSE, FWER=FALSE, return.selmodels = TRUE, return.nonaggr = TRUE,
-                                     args.lasso_inference = list(family = "binomial")), 0)
+                                     args.model.selector = list(standardize = FALSE, q = 16, intercept = TRUE, tol.beta = 1e-5),
+                                     verbose = FALSE, FWER = FALSE, return.selmodels = TRUE,
+                                     family = "binomial"), 0)
+    
+    out.list <- list()
+    out.list$y <- y
     if (!is.null(mcrtry$error) || !is.null(c100try$error)) {
       # error handling
       err <- paste("mcr:", mcrtry$error, "carve100:", c100try$error)
       war <- if (is.null(mcrtry$warning) || is.null(c100try$warning)) NA
       else c(mcrtry$warning, c100try$warning)
-      out_list <- list()
-      for (b in B_vec) {
+      
+      for (b in B.vec) {
         if (b > 1) {
-          out_list[[as.character(b)]] <- rep(NA, (length(ind) + 1) * 16)
+          out.list[[as.character(b)]] <- rep(NA, (length(ind) + 1) * 16)
         } else {
-          out_list[[as.character(b)]] <- rep(NA, (length(ind) + 1) * 6 + 9)
+          out.list[[as.character(b)]] <- rep(NA, (length(ind) + 1) * 6 + 9)
         }
       }
-      out_list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
-      out_list
+      out.list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
+      out.list
     } else {
       mcr <- mcrtry$value
-      pcarve_nofwer <- mcr[[1]]$pvals.nonaggr
-      psplit_nofwer <- mcr[[2]]$pvals.nonaggr
-      model_size <- apply(mcr[[1]]$sel.models, 1, sum)
-      model_size[model_size == 0]  <- 1 # if no variable is selected, p-values are 1
+      pcarve.nofwer <- mcr[[1]]$pvals.nonaggr
+      psplit.nofwer <- mcr[[2]]$pvals.nonaggr
+      model.size <- apply(mcr[[1]]$sel.models, 1, sum)
+      model.size[model.size == 0]  <- 1 # if no variable is selected, p-values are 1
       # ommit the clipping to calculate adjusted power
-      # pcarve_fwer <- pmin(pcarve_nofwer * model_size, 1)
-      # psplit_fwer <- pmin(psplit_nofwer * model_size, 1)
-      pcarve_fwer <- pcarve_nofwer*model_size
-      psplit_fwer <- psplit_nofwer*model_size
+      # pcarve.fwer <- pmin(pcarve.nofwer * model.size, 1)
+      # psplit.fwer <- pmin(psplit.nofwer * model.size, 1)
+      pcarve.fwer <- pcarve.nofwer*model.size
+      psplit.fwer <- psplit.nofwer*model.size
       c100 <- c100try$value
-      pc100_nofwer <- c100$pval.corr
-      model_size100 <- apply(c100$sel.models, 1, sum)
-      model_size100[model_size100 == 0]  <- 1
-      # pc100_fwer <- pmin(pc100_nofwer * model_size100, 1)
-      pc100_fwer <- pc100_nofwer * model_size100
+      pc100.nofwer <- c100$pval.corr
+      model.size100 <- sum(c100$sel.models)
+      model.size100[model.size100 == 0]  <- 1
+      # pc100.fwer <- pmin(pc100.nofwer * model.size100, 1)
+      pc100.fwer <- pc100.nofwer * model.size100
       
-      out_list<-list()
-      for (B in B_vec) {
+      for (B in B.vec) {
         if (B > 1) {
           use <- 1:B
-          pvals.aggregated <- pval.aggregator(list(pcarve_nofwer[use, ], pcarve_fwer[use, ], psplit_nofwer[use, ], psplit_fwer[use, ]),
+          pvals.aggregated <- pval.aggregator(list(pcarve.nofwer[use, ], pcarve.fwer[use, ], psplit.nofwer[use, ], psplit.fwer[use, ]),
                                               round(seq(ceiling(0.05 * B)/B, 1 - 1/B, by = 1/B), 2), cutoff = FALSE)
-          pvals.aggregated2 <- pval.aggregator(list(pcarve_nofwer[use, ], pcarve_fwer[use, ], psplit_nofwer[use, ], psplit_fwer[use, ]),
+          pvals.aggregated2 <- pval.aggregator(list(pcarve.nofwer[use, ], pcarve.fwer[use, ], psplit.nofwer[use, ], psplit.fwer[use, ]),
                                                round(seq(ceiling(0.3 * B)/B, 1 - 1/B, by = 1/B), 2), cutoff = FALSE)
-          pvals.aggregated3 <- pval.aggregator(list(pcarve_nofwer[use, ], pcarve_fwer[use, ], psplit_nofwer[use, ], psplit_fwer[use, ]),
+          pvals.aggregated3 <- pval.aggregator(list(pcarve.nofwer[use, ], pcarve.fwer[use, ], psplit.nofwer[use, ], psplit.fwer[use, ]),
                                                round(ceiling(0.05 * B)/B, 2), cutoff = FALSE)
-          pvals.aggregated4 <- pval.aggregator(list(pcarve_nofwer[use, ], pcarve_fwer[use, ], psplit_nofwer[use, ], psplit_fwer[use, ]),
+          pvals.aggregated4 <- pval.aggregator(list(pcarve.nofwer[use, ], pcarve.fwer[use, ], psplit.nofwer[use, ], psplit.fwer[use, ]),
                                                round(ceiling(0.3 * B)/B, 2), cutoff = FALSE)
         } else {
-          pvals.aggregated <- list(pcarve_nofwer[1, ], pcarve_fwer[1, ], psplit_nofwer[1, ], psplit_fwer[1, ])
+          pvals.aggregated <- list(pcarve.nofwer[1, ], pcarve.fwer[1, ], psplit.nofwer[1, ], psplit.fwer[1, ])
         }
         
         
-        run_res <- vector(length = 0) # store important quantities
+        run.res <- vector(length = 0) # store important quantities
         np <- length(pvals.aggregated)
         for (i in 1:np) {
-          true_pv <- pvals.aggregated[[i]][ind] # p-values of active variables
-          bad_pv <- min(pvals.aggregated[[i]][-ind]) # lowest p-value of inactive variables to check FWER
-          run_res <- c(run_res, true_pv, bad_pv)
+          true.pv <- pvals.aggregated[[i]][ind] # p-values of active variables
+          bad.pv <- min(pvals.aggregated[[i]][-ind]) # lowest p-value of inactive variables to check FWER
+          run.res <- c(run.res, true.pv, bad.pv)
         }
         if (B > 1) {
           for (i in 1:np) {
-            true_pv <- pvals.aggregated2[[i]][ind]
-            bad_pv <- min(pvals.aggregated2[[i]][-ind])
-            run_res <- c(run_res, true_pv, bad_pv)
+            true.pv <- pvals.aggregated2[[i]][ind]
+            bad.pv <- min(pvals.aggregated2[[i]][-ind])
+            run.res <- c(run.res, true.pv, bad.pv)
           }
           for (i in 1:np) {
-            true_pv <- pvals.aggregated3[[i]][ind]
-            bad_pv <- min(pvals.aggregated3[[i]][-ind])
-            run_res <- c(run_res, true_pv, bad_pv)
+            true.pv <- pvals.aggregated3[[i]][ind]
+            bad.pv <- min(pvals.aggregated3[[i]][-ind])
+            run.res <- c(run.res, true.pv, bad.pv)
           }
           for (i in 1:np) {
-            true_pv <- pvals.aggregated4[[i]][ind]
-            bad_pv <- min(pvals.aggregated4[[i]][-ind])
-            run_res <- c(run_res, true_pv, bad_pv)
+            true.pv <- pvals.aggregated4[[i]][ind]
+            bad.pv <- min(pvals.aggregated4[[i]][-ind])
+            run.res <- c(run.res, true.pv, bad.pv)
           }
         }
         if (B == 1) {
@@ -211,44 +185,46 @@ for (frac in frac_vec) {
           R <- length(which(mcr[[1]]$sel.models[1, ])) # number of variables selected in first split
           TS <- sum(ind %in% which(mcr[[1]]$sel.models[1, ])) # number of active variables selected
           V <- R - TS # number of inactive variables selected
-          carve_err <- sum(pvals.aggregated[[1]][-ind] < 0.05)
-          split_err <- sum(pvals.aggregated[[3]][-ind] < 0.05)
-          carve100_err <- sum(pc100_nofwer[-ind] < 0.05)
-          run_res <- c(run_res, R, V, TS) 
-          true_pv <- pc100_nofwer[ind]
-          bad_pv <- min(pc100_nofwer[-ind])
+          carve.err <- sum(pvals.aggregated[[1]][-ind] < 0.05)
+          split.err <- sum(pvals.aggregated[[3]][-ind] < 0.05)
+          carve100.err <- sum(pc100.nofwer[-ind] < 0.05)
+          run.res <- c(run.res, R, V, TS) 
+          true.pv <- pc100.nofwer[ind]
+          bad.pv <- min(pc100.nofwer[-ind])
           R100 <- length(which(c100$sel.models))
           TS100 <- sum(ind %in% which(c100$sel.models))
           V100 <- R100 - TS100
-          run_res <- c(run_res, true_pv, bad_pv)
-          true_pv <- pc100_fwer[ind]
-          bad_pv <- min(pc100_fwer[-ind])
-          run_res <- c(run_res, true_pv, bad_pv, R100, V100, TS100,
-                       carve_err, split_err, carve100_err)
+          run.res <- c(run.res, true.pv, bad.pv)
+          true.pv <- pc100.fwer[ind]
+          bad.pv <- min(pc100.fwer[-ind])
+          run.res <- c(run.res, true.pv, bad.pv, R100, V100, TS100,
+                       carve.err, split.err, carve100.err)
         }
-        out_list[[as.character(B)]] <- run_res
+        out.list[[as.character(B)]] <- run.res
       }
       err <- if (is.null(mcrtry$error) && is.null(c100try$error)) NA
       else c(mcrtry$error, c100try$error) # should not happen due to earlier check
       war <- if (is.null(mcrtry$warning) && is.null(c100try$warning)) NA
       else c(mcrtry$warning, c100try$warning)
-      out_list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
-      out_list
+      out.list$exception <- list(err, paste(1:length(war), ":", war, collapse = ", "))
+      out.list
       # end of simulation run
     }
     # end of simulation for given fraction
   }
   toc()
   stopCluster(cl)
-  if (local) {
-    close(pb)
-  }
+
   # analyse results for given fraction
   expmatr <- matrix(unlist(res[, "exception"]), nrow = dim(res)[1], ncol = 2, byrow = TRUE)
   print(sum(is.na(expmatr[, 1])))
   succ = which(is.na(expmatr[, 1]))
   print("succesful runs")
-  for (B in B_vec) {
+  
+  all.y <- matrix(unlist(res[,"y"]), nrow = dim(res), byrow = TRUE)
+  sd <- attr(res, "rng")
+  
+  for (B in B.vec) {
     if (B == 1) {
       names1 <- c("carve","carvefw", "split", "splitfw")
       names2 <- c("carve100","carve100fw")
@@ -258,7 +234,7 @@ for (frac in frac_vec) {
       subres <- subres[succ,]
       colnames(subres) <- c(rep(names1, each = (sparsity + 1)), "R", "V", "R-V",
                             rep(names2, each = (sparsity + 1)), "R100", "V100",
-                            "R-V100", "carve_err", "split_err", "carve100_err")
+                            "R-V100", "carve.err", "split.err", "carve100.err")
       names <- c(names1, names2)
     } else {
       names<-c("carve5", "carvefw5", "split5", "splitfw5", "carve30",
@@ -272,8 +248,8 @@ for (frac in frac_vec) {
       colnames(subres) <- c(rep(names, each = (sparsity + 1)))
     }
     subres <- as.data.frame(subres)
-    simulation <- list("results" = subres, "exceptions" = expmatr, "B" = B, "split" = frac,
-                       "nsim" = nsim, "seed" = rseed, "All used B" = B_vec)
+    simulation <- list("results" = subres, "exceptions" = expmatr, "y" = all.y, "B" = B, "split" = frac,
+                       "nsim" = nsim, "seed" = rseed, "All used B" = B.vec, "sd" = sd, "commit" = commit)
     print(paste("results using fraction ", frac, " and B=", B, sep = ""))
     if (B == 1) {
       print(mean(subres$`R-V` == sparsity)) # probability of screening
@@ -284,11 +260,11 @@ for (frac in frac_vec) {
       good100 <- which(subres$`R-V100` == sparsity)
       # totally, active and inacitve selected using all data for selection
       print(apply(subres[, c("R100", "V100", "R-V100")], 2, mean))
-      print(c(c(sum(subres$carve_err), sum(subres$split_err)) / sum(subres$V),
-              sum(subres$carve100_err) / sum(subres$V100))) # Rejection amongst falsely selected
+      print(c(c(sum(subres$carve.err), sum(subres$split.err)) / sum(subres$V),
+              sum(subres$carve100.err) / sum(subres$V100))) # Rejection amongst falsely selected
       # Rejection amongst falsely selected conditioned on screening, should be below 0.05
-      print(c(c(sum(subres$carve_err[good]), sum(subres$split_err[good])) / sum(subres$V[good]),
-              sum(subres$carve100_err[good100]) / sum(subres$V100[good100]))) 
+      print(c(c(sum(subres$carve.err[good]), sum(subres$split.err[good])) / sum(subres$V[good]),
+              sum(subres$carve100.err[good100]) / sum(subres$V100[good100]))) 
     } 
     allrej <- matrix(NA, nrow = 1,ncol = length(names))
     colnames(allrej) <- names
@@ -317,14 +293,8 @@ for (frac in frac_vec) {
     resname <- paste0("results ", format(Sys.time(), "%d-%b-%Y %H.%M"),
                       " split=", frac, " B=", B, " seed=", rseed)
     # adjust depending on folder structure
-    if (save) {
-      if (local) {
-        save(simulation, file = paste("C:/Users/Christoph/Documents/ETH/MA/R_trials/multi_carve/",
-                                      newdir, "/", resname, ".RData", sep = ""))
-      } else {
-        save(simulation,file = paste("multi_carve/", newdir, "/", resname, ".RData", sep = ""))
-      }
-    }
+    if (save) save(simulation,file = paste("multi_carve/", newdir, "/", resname, ".RData", sep = ""))
+      
   }
 }
 
